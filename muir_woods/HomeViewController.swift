4cbc0bedf4fd3b7dfe718f23d9ff85acb29d2c8b
FruitDragon1a <fruit.dragonfruit@gmail.com>
2024-04-24 21:34:38 -0700

    idke

--- muir_woods/HomeViewController.swift
+++ muir_woods/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  muir_woods
 //
 //  Created by Ryka Jain on 4/24/24.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ muir_woods/MapViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MapViewController.swift
+//  muir_woods
+//
+//  Created by Ryka Jain on 4/24/24.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let initialLocation = CLLocation(latitude: 37.896980, longitude: -122.581711)
+        let regionRadius: CLLocationDistance = 10000.0
+        let region = MKCoordinateRegion(center: initialLocation.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(region, animated: true)
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
